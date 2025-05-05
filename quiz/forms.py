fde12aba0e7d43e7e122333d31027753a330f89b
Armine1203 <arminelalayan2003@gmail.com>
2025-05-06 02:36:31 +0400

    field name changes in Create exam form

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Subject, Major, MidtermExam
+from .models import Subject, Major, MidtermExam, Group
 
 
 class SubjectForm(forms.ModelForm):
@@ -19,16 +19,35 @@ class SubjectForm(forms.ModelForm):
 class MidtermExamForm(forms.ModelForm):
     class Meta:
         model = MidtermExam
-        fields = ['subject', 'group', 'questions','due_date', 'time_limit']
+        fields = ['subject', 'group', 'questions', 'due_date', 'time_limit']
         widgets = {
-            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'questions': forms.CheckboxSelectMultiple(),
+            'due_date': forms.DateTimeInput(attrs={
+                'type': 'datetime-local',
+                'class': 'form-control'
+            }),
+            'time_limit': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'min': 1,
+                'max': 180
+            }),
+            'subject': forms.Select(attrs={'class': 'form-control'}),
+            'group': forms.Select(attrs={'class': 'form-control'}),
+            'questions': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'})
+        }
+        labels = {
+            'subject': 'Առարկա',
+            'group': 'Խումբ',
+            'questions': 'Հարցեր',
+            'due_date': 'Ավարտի ամսաթիվ և ժամ',
+            'time_limit': 'Քննության տևողություն (րոպե)'
         }
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)  # user-ը ստանում ենք view-ից
+        user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
 
         if user is not None:
-            # Ֆիլտրենք subject-ը՝ ըստ դասախոսի
             self.fields['subject'].queryset = Subject.objects.filter(lecturers=user)
+            self.fields['group'].queryset = Group.objects.filter(
+                major__in=self.fields['subject'].queryset.values('major')
+            )
